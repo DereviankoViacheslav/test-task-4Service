d4def821415cd4dce813192ff4da5ed9a0a05142
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2020-02-12 17:57:21 +0200

    Updates

---
+++ precache-manifest.f6f2d398ebc6e8db4c19f8a235ac9a4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521fcd64f3f7b325f5f8e0660dcb2e21",
+    "url": "/test-task-4Service/index.html"
+  },
+  {
+    "revision": "499f47c5527d674213ee",
+    "url": "/test-task-4Service/static/css/main.0e7cc71e.chunk.css"
+  },
+  {
+    "revision": "060c40bd7d1f652afc34",
+    "url": "/test-task-4Service/static/js/2.6017274f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/test-task-4Service/static/js/2.6017274f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "499f47c5527d674213ee",
+    "url": "/test-task-4Service/static/js/main.abc0da4d.chunk.js"
+  },
+  {
+    "revision": "cd4102a6c4d1a795b027",
+    "url": "/test-task-4Service/static/js/runtime-main.08f3f3f6.js"
+  }
+]);
---
+++ static/js/main.abc0da4d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonptest-task-4service"]=this["webpackJsonptest-task-4service"]||[]).push([[0],{3:function(e,t,n){e.exports=n(9)},8:function(e,t,n){},9:function(e,t,n){"use strict";n.r(t);var r=n(0),s=n.n(r),c=n(2),a=n.n(c);n(8);var o=function(){return s.a.createElement("div",null,"Hello")};a.a.render(s.a.createElement(o,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.abc0da4d.chunk.js.map

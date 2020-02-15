cc4a06635e9f6c81892fbe2bdfa738500a048951
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2020-02-15 14:01:47 +0200

    Updates

---
+++ precache-manifest.4225c92368a79e1092eb52113657a6c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11142c0b1c13495a5a16bc931d43324",
+    "url": "/test-task-4Service/index.html"
+  },
+  {
+    "revision": "b5e43e3b92be35f192e2",
+    "url": "/test-task-4Service/static/css/main.8685c42b.chunk.css"
+  },
+  {
+    "revision": "7b97ddc865dc0757bed1",
+    "url": "/test-task-4Service/static/js/2.72dfe2c0.chunk.js"
+  },
+  {
+    "revision": "a5853dc969c0b63ac51782b388585f5d",
+    "url": "/test-task-4Service/static/js/2.72dfe2c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5e43e3b92be35f192e2",
+    "url": "/test-task-4Service/static/js/main.17055adc.chunk.js"
+  },
+  {
+    "revision": "cd4102a6c4d1a795b027",
+    "url": "/test-task-4Service/static/js/runtime-main.08f3f3f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test-task-4Service/precache-manifest.7c0504dc0e8fb3cb89d52874bf47c6c7.js"
+  "/test-task-4Service/precache-manifest.4225c92368a79e1092eb52113657a6c7.js"
 );
 
 self.addEventListener('message', (event) => {
